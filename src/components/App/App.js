292280f42094c7cc307093fe53dca68d6147cff8
Jack Taylor <jack.s.o.taylor@googlemail.com>
2021-02-10 13:56:59 +0000

    Removed unused code from App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,13 @@
-import { useState } from "react";
-
-import { deleteToDo, addToDo } from "../../api";
 import Header from "../Header";
 import ToDoList from "../ToDoList";
 import Form from "../Form";
 
 const App = () => {
-  const [toDos, setToDos] = useState([]);
-
-  const handleAddNew = async text => {
-    const res = await addToDo(text);
-    setToDos(res);
-  };
-
   return (
     <>
       <Header />
       <ToDoList />
-      <Form handleSubmit={handleAddNew} />
+      <Form />
     </>
   );
 };
