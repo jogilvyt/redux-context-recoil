10822df3d5b9636b1ebf1750c19183d9b13fb33a
Jack Taylor <jack.s.o.taylor@googlemail.com>
2021-02-10 12:20:54 +0000

    Removed unused code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { deleteToDo, addToDo } from "../../api";
 import { fetchToDos } from "../../state/actions";
 import Header from "../Header";
 import ToDoList from "../ToDoList";
